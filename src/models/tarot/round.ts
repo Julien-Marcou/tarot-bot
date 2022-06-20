68a52544cc01a5fce88f5c61ddf4d6adfe89bb5e
Julien Marcou <julien.marcou.dev@gmail.com>
2022-06-20 22:47:47 +0200

    Improve round typing

--- src/models/tarot/round.ts
+++ src/models/tarot/round.ts
@@ -2,11 +2,19 @@ import type { Bid } from './bid';
 import type { Card } from './card';
 import type { Player, PlayerPosition } from './player';
 
-export type Round = {
+type BaseRound = {
   leadPosition: PlayerPosition;
   currentPosition: PlayerPosition;
-  trick?: Array<Card>;
-  bids?: Array<Bid>;
+};
+
+export type BiddingRound = BaseRound & {
+  bids: Array<Bid>;
+};
+
+export type TrickRound = {
+  trick: Array<Card>;
   trickWinner?: Player;
   foolReceiver?: Player;
 };
+
+export type Round = BiddingRound | TrickRound;
